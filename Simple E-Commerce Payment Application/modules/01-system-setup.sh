041edd05ee857b3620cb2374eee43711f8d8347c
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-01-31 23:58:38 +0500

    updated system-setup.sh

--- Simple E-Commerce Payment Application/modules/01-system-setup.sh
+++ Simple E-Commerce Payment Application/modules/01-system-setup.sh
@@ -15,8 +15,17 @@ if [[ $EUID -ne 0 ]]; then
   exit 1
 fi
 
-echo -e "${YELLOW}[*] Updating System Package${NC}"
+echo -e "${YELLOW}[*] Updating System Package....${NC}"
 apt-get update
 apt-get upgrade -y
 
-echo -e "${YELLOW}[*] Installing Packages"
+echo -e "${YELLOW}[*] Installing Packages....${NC}"
+apt-get install -y \
+    net-tools \
+    nginx \
+    apache2 apache2-utils libapache2-mod-php \
+    mysql-server \
+    php8.3 php8.3-fpm php8.3-mysql php8.3-common
+
+echo -e "${GREEN}[+] System setup Compeleted${NC}"
+echo ""
